9081ea65c91564f4e4ed5d4597600f621e6d1551
snekavsb <31505096+snekavsb@users.noreply.github.com>
2018-01-23 10:16:05 +0000

    Create multi7.c

---
+++ multi7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d\n",&n);
+	if(n%7==0)
+	{
+		printf("yes multiple of 7");
+	}
+	else
+	{
+		printf("not multiple of 7");
+	}
+	return 0;
+}
