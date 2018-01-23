ab898fcf97fa714843e76ad78764402496e9693e
snekavsb <31505096+snekavsb@users.noreply.github.com>
2018-01-23 10:51:07 +0000

    Create mul13.c

---
+++ mul13.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,b;
+	scanf("%d\n",&a);
+	printf("%d\n",a);
+	if(a%13==0)
+	{
+		printf("mulitiple of 13");
+	}
+	else
+	
+	{
+		printf("not multiple of 13");
+	}
+	return 0;
+}
