a536b7693f262f294bf7bf33afac8bfc81b6b9e2
snekavsb <31505096+snekavsb@users.noreply.github.com>
2018-01-23 11:21:54 +0000

    Create *abc.c

---
+++ *abc.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,b,c;
+	float d;
+	scanf("%d\n%d\n%d\n",&a,&b,&c);
+	printf("%d\n%d\n%d\n",a,b,c);
+	d=((a*b)/c);
+	printf("%f\n",d);
+	
+	
+	return 0;
+}
