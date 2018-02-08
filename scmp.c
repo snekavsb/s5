00b71355c6944e6b1f149a2ee48bbe1c688552c1
snekavsb <31505096+snekavsb@users.noreply.github.com>
2018-02-08 14:12:09 +0530

    Create scmp.c

---
+++ scmp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	 char a[56],b[56];
+	 scanf("%s\n%s\n",&a,&b);
+	 printf("%s\n%s\n",a,b);
+	 if(a>b)
+	 {
+	 	printf("%s\n",a);
+	 }
+	 else
+	 {
+	 	printf("%s\n",b);
+	 }
+	return 0;
+}
