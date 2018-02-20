225674f6c73ab86f5100ce8a6de24a96bb69d7c1
snekavsb <31505096+snekavsb@users.noreply.github.com>
2018-02-20 16:53:30 +0530

    Create ps.c

---
+++ ps.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,b,c;
+	scanf("%d\n%d\n",&a,&b);
+c=a*b;
+		if(c/a==b)
+		{
+			printf("perfect square");
+		}
+		else
+		{
+			printf("not");
+		}
+	return 0;
+}
