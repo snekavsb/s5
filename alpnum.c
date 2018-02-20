fb9a7227e56093794379eafc8fdce6df634c45f4
snekavsb <31505096+snekavsb@users.noreply.github.com>
2018-02-20 16:33:55 +0530

    Create alpnum.c

---
+++ alpnum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+char i;
+scanf("%c\n",i);
+if(((i>'a'||i<'z')||(i>'A'||i<'Z'))||(i>1||i<1000))
+{
+	printf("both alphabets and numbers");
+}
+
+else
+{}
+	return 0;
+}
