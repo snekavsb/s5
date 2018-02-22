a538ce20b0c2d7d4bcb8b6cc7b3bd98e4e3d3fd5
snekavsb <31505096+snekavsb@users.noreply.github.com>
2018-02-22 17:00:07 +0530

    Create spdi.c

---
+++ spdi.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	char a[65];
+	int i,s=0,f;
+	scanf("%s",&a[i]);
+	for(i=0;i!='\0';i++)
+	{
+		if(a[i]>0&&a[i]<9&&a[i]>'a'&&a[i]<'z'&&a[i]>'A'&&a[i]<'Z')
+		{
+			
+		}
+		else
+	}
+	
+	return 0;
+}
