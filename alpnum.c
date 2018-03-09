09833c4977923890b456ccbbfc5e2bbbac64c398
snekavsb <31505096+snekavsb@users.noreply.github.com>
2018-03-09 15:40:12 +0530

    Update alpnum.c

--- alpnum.c
+++ alpnum.c
@@ -3,7 +3,7 @@
 int main(void) {
 char i;
 scanf("%c\n",i);
-if(((i>'a'||i<'z')||(i>'A'||i<'Z'))||(i>1||i<1000))
+if(((i>'a'&&i<'z')||(i>'A'&&i<'Z'))||(i>1&&i<1000))
 {
 	printf("both alphabets and numbers");
 }
